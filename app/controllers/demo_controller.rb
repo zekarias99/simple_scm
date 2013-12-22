644e5fa3afd08f07b3ae019dea786047c90a9572
Zekarias Araya <zekarias99@gmail.com>
2013-12-21 16:53:01 -0800

    Render action

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -3,5 +3,11 @@ class DemoController < ApplicationController
   layout false # This will supress not to use the layout.
 
   def index # This is colled Action 
+  	render 'hello'
   end
+
+  def hello
+  	render 'index'
+  end
+
 end
