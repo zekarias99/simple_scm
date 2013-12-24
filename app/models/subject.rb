0f12e1889355634caba1bbcd397a72406e3c156e
Zekarias Araya <zekarias99@gmail.com>
2013-12-23 19:14:52 -0800

    Name scopes

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -1,2 +1,6 @@
 class Subject < ActiveRecord::Base
+	scope :visible,      lambda { where(:visible => true)}
+	scope :invisible,    lambda { where(:visible => false) }
+	scope :sorted,       lambda { order("subjects.position ASC")}
+	scope :newest_first, lambda { order("subjects.created_at DESC")}
 end
