a0150cbfbd4826fa0ea8da09890f0a4eb35db886
Zekarias Araya <zekarias99@gmail.com>
2013-12-28 17:59:33 -0800

    Create action: New

--- app/controllers/subjects_controller.rb
+++ app/controllers/subjects_controller.rb
@@ -7,9 +7,11 @@ class SubjectsController < ApplicationController
 	end
 
 	def show
+		@subject = Subject.find(params[:id])
 	end
 
 	def new
+		@subject = Subject.new({:name => "Default"})
 	end
 
 	def edit
