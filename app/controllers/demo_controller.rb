c9acce812bf18e2389e2f2d83ba07ff89d3b636b
Zekarias Araya <zekarias99@gmail.com>
2014-01-02 04:13:36 -0800

    JavaScript

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,6 +1,6 @@
 class DemoController < ApplicationController
   
-  layout false # This will supress not to use the layout.
+  layout 'application'  #layout false will supress not to use the layout.
 
   def index # This is colled Action 
   end 
